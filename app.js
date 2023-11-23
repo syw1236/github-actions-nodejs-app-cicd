2fa70ef89ecf0b4cad77a12e785423ab10290288
YeWon Seo <syw1236@naver.com>
2023-11-24 03:26:41 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // app.js
-const http = require('http');
+const http = require("http");
 
-const hostname = '0.0.0.0';
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!!!!\n");
 });
 
 server.listen(port, hostname, () => {
